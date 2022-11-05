ee77b424794eb2bcb038ca45def7e7ae961c0000
kobanya <nagy.bela.budapest@gmail.com>
2022-11-05 08:33:09 +0100

    widget-be helyezzük az előzőleg megírt kódot. Az új osztály neve : Kezdo

--- lib/main.dart
+++ lib/main.dart
@@ -2,34 +2,43 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   // a fejléc szövege és annak
-  home: Scaffold(
-     appBar: AppBar(
-       title:  const Text (
-            "Az első mobilalkalmazásom",
-       style: TextStyle(fontWeight: FontWeight.bold
-  ),
-  ),
-       centerTitle: true,
-       backgroundColor: Colors.lightGreen,
-// az alakalmazás teste, beállításokkal
-  ),
-    body: const Center (
-       child: Text (
-        "Az első mondatom",
-        style: TextStyle(
-        fontSize: 20.0,
-        fontWeight: FontWeight.bold,
-        color: Colors.deepOrange,
-
-      ),
-      ),
-    ),
-      floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      backgroundColor:  Colors.blueGrey,
-      child: const Text("Katt"),
-    ),
-  ),
+  home: Kezdo()
 ));
+
+class Kezdo extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            title:  Text (
+              "Az első mobilalkalmazásom",
+            style: TextStyle
+                     (fontWeight: FontWeight.bold
+              ),
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.lightGreen,
+// az alakalmazás teste, beállításokkal
+          ),
+          body: const Center (
+            child: Text (
+              "Az első mondatom",
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepOrange,
+
+              ),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {  },
+            backgroundColor:  Colors.blueGrey,
+            child: const Text("Katt"),
+          ),
+        );
+      }
+    }
+
 
 
